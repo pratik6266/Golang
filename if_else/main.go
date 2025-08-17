4f10b7dda3873b85e9f68a81ce854d14fde133c7
Pratik Raj <pratikraj220011@gmail.com>
2025-08-17 16:47:22 +0530

    Refactor conditional statements and add switch case implementation in Go

--- if_else/main.go
+++ if_else/main.go
@@ -14,7 +14,29 @@ func main() {
 		fmt.Println("x is greater than y")
 	} else if x == y {
 		fmt.Println("x is equal to y")	
-	} else {
+	} else {    
 		fmt.Println("x is not greater than y")
+	}
+
+	fmt.Printf("\n-------------------------------------\n\n")
+
+	day := 8;
+	switch (day) {
+		case 1:
+			fmt.Println("Monday")
+		case 2:
+			fmt.Println("Tuesday")
+		case 3:
+			fmt.Println("Wednesday")
+		case 4:	
+			fmt.Println("Thursday")
+		case 5:
+			fmt.Println("Friday")
+		case 6:
+			fmt.Println("Saturday")
+		case 7:
+			fmt.Println("Sunday")
+		default:
+			fmt.Println("Invalid day")
 	}
 }
